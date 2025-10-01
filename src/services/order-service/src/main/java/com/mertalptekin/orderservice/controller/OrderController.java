835392c5ac255974ef936e8183672886456da931
neominal.solutions <neominal.solutions@gmail.com>
2025-10-01 14:09:48 +0300

    Order Service

--- src/services/order-service/src/main/java/com/mertalptekin/orderservice/controller/OrderController.java
+++ src/services/order-service/src/main/java/com/mertalptekin/orderservice/controller/OrderController.java
@@ -6,12 +6,15 @@ import com.mertalptekin.orderservice.application.client.ProductClient;
 import com.mertalptekin.orderservice.application.dto.CreateOrderRequest;
 import com.mertalptekin.orderservice.application.dto.GetOrderedProductRequest;
 import com.mertalptekin.orderservice.application.dto.OrderedProductReponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -31,10 +34,21 @@ public class OrderController {
 
     // api/v1/orders -> POST isteği
     @PostMapping
+    @CircuitBreaker(name = "productClient",fallbackMethod = "getOrderedProductsCircuitBraker")
     public  ResponseEntity<OrderedProductReponse> getOrderedProducts(@RequestBody GetOrderedProductRequest request){
         System.out.println("Order Servis");
        return productClient.getOrderedProducts(request);
     }
+
+    public ResponseEntity<OrderedProductReponse> getOrderedProductsCircuitBraker(Exception ex)  {
+        // logger
+        System.out.println("getOrderedProductsCircuitBrakerFallback");
+        // hata dışında eğer veri cachledeye response son güncel cache üzerinden döner.
+        return ResponseEntity.ok(new OrderedProductReponse(List.of()));
+
+    };
+
+    // herhangi bir hata durumunda productService erişemezsek o zaman response olarak ne göstereceğiz?
 
     // Bunu test etmek için 1-Eureka Server çalışmalı, 2-OrderService, 3-ProductService
     // Product Service Config ayarlar içinde mecbur Config Serverda açık olmalı. Run
