faeedf49032bc3f8b499ff4b8c1b29af207466fa
neominal.solutions <neominal.solutions@gmail.com>
2025-10-01 16:21:48 +0300

    GatewayService

--- src/services/product-service/src/main/java/com/mertalptekin/productservice/application/dto/GetOrderedProductRequest.java
+++
@@ -1,6 +0,0 @@
-package com.mertalptekin.productservice.application.dto;
-
-import java.util.List;
-
-// Request -> OrderId için Productları döndür
-public record GetOrderedProductRequest(List<String> ProductIds, String OrderId){}
