b27be050dee8e4d60e0760e335a8aaf1e6c187e8
neominal.solutions <neominal.solutions@gmail.com>
2025-10-01 10:40:18 +0300

    Product Services Ordered Products Model

---
+++ src/services/product-service/src/main/java/com/mertalptekin/productservice/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.mertalptekin.productservice.controller;
+
+
+// Siparişe düşen ürünleri yönettiğimiz contoller
+
+import com.mertalptekin.productservice.application.dto.GetOrderedProductRequest;
+import com.mertalptekin.productservice.application.dto.OrderProductResponse;
+import com.mertalptekin.productservice.service.model.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.EntityResponse;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+
+
+    @PostMapping
+    public ResponseEntity<OrderProductResponse> getOrderedProducts(@RequestBody GetOrderedProductRequest request) {
+
+        System.out.println(request.OrderId());
+        System.out.println(request.ProductIds());
+
+        List<Product> plist = List.of(new Product("1","P-1", BigDecimal.valueOf(10.5),20),new Product("2","P-2"
+        ,BigDecimal.valueOf(30),40));
+
+      return   ResponseEntity.ok(new OrderProductResponse(plist));
+
+    }
+
+
+}
---
+++ src/services/product-service/src/main/java/com/mertalptekin/productservice/service/model/Product.java
@@ -0,0 +1,20 @@
+package com.mertalptekin.productservice.service.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    private String id;
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+
+}
